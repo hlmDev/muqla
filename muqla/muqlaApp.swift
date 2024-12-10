005b114b062665ba33528a1c23b06aa15903fd56
hlmDev <a7m.almu@gmail.com>
2024-12-10 15:25:29 +0300

    Initial Commit

---
+++ muqla/muqlaApp.swift
@@ -0,0 +1,17 @@
+//
+//  muqlaApp.swift
+//  muqla
+//
+//  Created by Ahlam Majed on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct muqlaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ muqlaTests/muqlaTests.swift
@@ -0,0 +1,17 @@
+//
+//  muqlaTests.swift
+//  muqlaTests
+//
+//  Created by Ahlam Majed on 10/12/2024.
+//
+
+import Testing
+@testable import muqla
+
+struct muqlaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
